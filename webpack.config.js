f2d941121a328c3aef4bf0c1c036a83ace464de5
David Clark <davidclark314@gmail.com>
2017-03-21 15:43:59 -0600

    Lots of changes to get master updated with new package versions. Also included new packages like flow, different babel configurations, etc.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
+const path = require('path')
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
-};
+	entry: [
+		'./src/index.js'
+	],
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	devServer: {
+		contentBase: __dirname,
+		historyApiFallback: true,
+		port: 8080
+	}
+}
